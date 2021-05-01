f6454faea0bf36a1a17d03286fa8c972b118ae60
icecream17 <58114641+icecream17@users.noreply.github.com>
2021-05-01 18:24:15 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class ExternalLink extends React.Component {
+   render() {
+      return (
+         <a
+            className="App-link"
+            href={this.props.href}
+            target="_blank"
+            rel="noopener noreferrer"
+         ></a>
+      )
+   }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <ExternalLink href="https://reactjs.org">
+               Learn react
+            </ExternalLink>
+         </header>
+      </div>
+   );
 }
 
 export default App;

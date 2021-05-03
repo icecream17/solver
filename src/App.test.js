35db1e53658e746ff3b32d0a70679ac15a5ffdbb
steven nguyen <icecream17.github@gmail.com>
2021-05-03 07:50:25 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { getGlobalRef } from './globalRef'
 
-test('renders learn react link', () => {
-   render(<App />);
-   const linkElement = screen.getByText(/learn react/i);
-   expect(linkElement).toBeInTheDocument();
+render(<App />);
+
+test('app exists', () => {
+   expect(document.getElementsByClassName('App')).toHaveLength(1)
+})
+
+describe('header', () => {
+   const AppHeaders = document.getElementsByClassName('App-header')
+
+   test('it exists', () => {
+      expect(AppHeaders).toHaveLength(1)
+      expect(AppHeaders[0].tagName).toBe('HEADER')
+   })
+   
+   test('has title and version', () => {
+      expect(AppHeaders[0].children).toHaveLength(2)
+   })
 });
+
+describe('main', () => {
+   test('it exists', () => {
+      expect(document.getElementsByClassName('App-main')).toHaveLength(1)
+   })
+})
+
+describe('aside', () => {
+   test('it exists', () => {
+      expect(document.getElementsByClassName('App-aside')).toHaveLength(1)
+   })
+
+   test('globalRef exists', () => {
+      expect(getGlobalRef('Aside')).not.toBeUndefined()
+   })
+})

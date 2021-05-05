511cb87897e75319efe48264d066ce1017a0d7c2
steven nguyen <icecream17.github@gmail.com>
2021-05-05 14:16:15 -0500

    CreateStrategyControls.js [skip ci]

---
+++ src/Elems/AsideElems/StrategyControls.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * A bunch of strategy controls
+ */
+export default class StrategyControls extends React.Component {
+   render() {
+      return (
+         <fieldset className='StrategyControls'>
+            <legend>controls</legend>
+            <!-- TODO -->
+         </fieldset>
+      )
+   }
+}

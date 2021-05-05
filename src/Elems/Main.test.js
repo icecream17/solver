0317729219e63bcc257c5de3fe7a79e73fcc130e
steven nguyen <icecream17.github@gmail.com>
2021-05-05 08:05:16 -0500

    debug

--- src/Elems/Main.test.js
+++ src/Elems/Main.test.js
@@ -1,116 +1,127 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event'
 import React from 'react'
 
 import Main from './Main'
 
-beforeAll(async () => {
-   await render(<Main />)
+render(<Main />)
+
+test.todo('more tests')
+
+const mainElement = document.getElementsByClassName('App-main')?.[0]
+const dataElement = mainElement?.children?.[0]
+const sudokuElement = mainElement?.children?.[1]
+
+if (!(mainElement && dataElement && sudokuElement)) {
+   screen.debug()
+}
+
+test('mainElement exists', () => {
+   expect(document.getElementsByClassName('App-main')).toHaveLength(1)
 })
 
-describe('Main', () => {
-   test.todo('more tests')
+test('and has 3 children', () => {
+   expect(mainElement.children).toHaveLength(3)
+})
 
-   test('it exists', () => {
-      expect(document.getElementsByClassName('App-main')).toHaveLength(1)
-   })
+test('dataElement.id === "Data"', () => {
+   expect(dataElement.id).toBe("Data")
+})
 
-   const mainElement = document.getElementsByClassName('App-main')[0]
-
-   test('has 3 children', () => {
-      expect(mainElement.children).toHaveLength(3)
-   })
-
-   const [dataElement, sudokuElement, _coordsElement] = mainElement.children
-   
-   test('dataElement.id === "Data"', () => {
-      expect(dataElement.id).toBe("Data")
-   })
-
-   describe('functionality', () => {
-      describe('cell changing', () => {
+describe.skip('functionality', () => {
+   describe('cell changing', () => {
+      function getCell (x, y) {
          // sudoku > tbody > row > cell
-         const firstCell = sudokuElement.children[0].children[0].children[0] // implicit structure test
+         sudokuElement.children[0].children[x].children[y] // implicit structure test
+      }
 
-         function candidatesOf(cell) {
-            return cell.innerText.replaceAll(/\n|\t/g, '')
-         }
+      function candidatesOf(cell) {
+         return cell.innerText.replaceAll(/\n|\t/g, '')
+      }
 
-         // candidates in the textbox
-         function displayedCandidates() {
-            return document.getElementById('Data').value
-         }
+      // candidates in the textbox
+      function displayedCandidates() {
+         return document.getElementById('Data').value
+      }
 
-         test('candidates pop up when focused', () => {
-            expect(() => firstCell.focus()).not.toThrow()
-            expect(candidatesOf(firstCell)).toBe('123456789')
-         })
+      test('candidates pop up when focused', () => {
+         const firstCell = getCell(0, 0)
 
-         // Implicit .not.toThrow for all keypress events
-         const keypressEvents = [
-            new KeyboardEvent('keypress'),
-            new KeyboardEvent('keypress', { key: '7' }),
-            new KeyboardEvent('keypress', { key: '3' }),
-            new KeyboardEvent('keypress', { key: '1' }),
-            new KeyboardEvent('keypress', { key: '4' }),
-            new KeyboardEvent('keypress', { key: '2' }),
-
-            new KeyboardEvent('keypress', { key: '6' }),
-            new KeyboardEvent('keypress', { key: '5' }),
-            new KeyboardEvent('keypress', { key: '3' }),
-            new KeyboardEvent('keypress', { key: '5' }),
-            new KeyboardEvent('keypress', { key: '3' }),
-
-            new KeyboardEvent('keypress', { key: '5' }),
-            new KeyboardEvent('keypress', { key: '8' }),
-            new KeyboardEvent('keypress', { key: '9' }),
-            new KeyboardEvent('keypress', { key: '4' }),
-         ]
-
-         function tryEvent(index) {
-            firstCell.dispatchEvent(keypressEvents[index])
-         }
-
-         test('candidates are toggled on keypress & textarea is updated', () => {
-            tryEvent(0);
-            expect(candidatesOf(firstCell)).toBe('123456789')
-            expect(displayedCandidates()).toBe('123456789')
-
-            tryEvent(1);
-            expect(candidatesOf(firstCell)).toBe('12345689')
-            expect(displayedCandidates()).toBe('89')
-
-            tryEvent(2); tryEvent(3); tryEvent(4);
-            tryEvent(5); tryEvent(6); tryEvent(7);
-            expect(candidatesOf(firstCell)).toBe('89')
-            expect(displayedCandidates()).toBe('89')
-
-            tryEvent(8); tryEvent(9);
-            expect(candidatesOf(firstCell)).toBe('8935')
-            expect(displayedCandidates()).toBe('8935')
-         })
-
-         test('when a cell is empty', () => {
-            tryEvent(10); tryEvent(11); tryEvent(12); tryEvent(13);
-            expect(candidatesOf(firstCell)).toBe('')
-            expect(displayedCandidates()).toBe('empty!')
-
-            firstCell.blur()
-            expect(candidatesOf(firstCell)).toBe('0')
-         })
-
-         test('when a cell has 1 candidate', () => {
-            firstCell.focus()
-            tryEvent(14);
-            expect(candidatesOf(firstCell)).toBe('4')
-            expect(displayedCandidates()).toBe('4')
-
-            firstCell.blur()
-            expect(firstCell.innerText).toBe('4') // No table!
-
-            firstCell.focus()
-            expect(firstCell.innerText).not.toBe('4') // Candidate table!
-         })
+         expect(() => firstCell.focus()).not.toThrow()
+         expect(candidatesOf(firstCell)).toBe('123456789')
       })
 
+      // Implicit .not.toThrow for all keypress events
+      const keypressEvents = [
+         new KeyboardEvent('keypress'),
+         new KeyboardEvent('keypress', { key: '7' }),
+         new KeyboardEvent('keypress', { key: '3' }),
+         new KeyboardEvent('keypress', { key: '1' }),
+         new KeyboardEvent('keypress', { key: '4' }),
+         new KeyboardEvent('keypress', { key: '2' }),
+
+         new KeyboardEvent('keypress', { key: '6' }),
+         new KeyboardEvent('keypress', { key: '5' }),
+         new KeyboardEvent('keypress', { key: '3' }),
+         new KeyboardEvent('keypress', { key: '5' }),
+         new KeyboardEvent('keypress', { key: '3' }),
+
+         new KeyboardEvent('keypress', { key: '5' }),
+         new KeyboardEvent('keypress', { key: '8' }),
+         new KeyboardEvent('keypress', { key: '9' }),
+         new KeyboardEvent('keypress', { key: '4' }),
+      ]
+
+      function tryEvent(index) {
+         firstCell.dispatchEvent(keypressEvents[index])
+      }
+
+      test('candidates are toggled on keypress & textarea is updated', () => {
+         const firstCell = getCell(0, 0)
+
+         tryEvent(0);
+         expect(candidatesOf(firstCell)).toBe('123456789')
+         expect(displayedCandidates()).toBe('123456789')
+
+         tryEvent(1);
+         expect(candidatesOf(firstCell)).toBe('12345689')
+         expect(displayedCandidates()).toBe('89')
+
+         tryEvent(2); tryEvent(3); tryEvent(4);
+         tryEvent(5); tryEvent(6); tryEvent(7);
+         expect(candidatesOf(firstCell)).toBe('89')
+         expect(displayedCandidates()).toBe('89')
+
+         tryEvent(8); tryEvent(9);
+         expect(candidatesOf(firstCell)).toBe('8935')
+         expect(displayedCandidates()).toBe('8935')
+      })
+
+      test('when a cell is empty', () => {
+         const firstCell = getCell(0, 0)
+
+         tryEvent(10); tryEvent(11); tryEvent(12); tryEvent(13);
+         expect(candidatesOf(firstCell)).toBe('')
+         expect(displayedCandidates()).toBe('empty!')
+
+         firstCell.blur()
+         expect(candidatesOf(firstCell)).toBe('0')
+      })
+
+      test('when a cell has 1 candidate', () => {
+         const firstCell = getCell(0, 0)
+
+         firstCell.focus()
+         tryEvent(14);
+         expect(candidatesOf(firstCell)).toBe('4')
+         expect(displayedCandidates()).toBe('4')
+
+         firstCell.blur()
+         expect(firstCell.innerText).toBe('4') // No table!
+
+         firstCell.focus()
+         expect(firstCell.innerText).not.toBe('4') // Candidate table!
+      })
    })
+
 })

3003d35bee3cce70e3e421186b43a7d1574972b8
steven nguyen <icecream17.github@gmail.com>
2021-05-06 11:15:47 -0500

    pass callback to cell [skip ci]

--- src/Elems/MainElems/Row.js
+++ src/Elems/MainElems/Row.js
@@ -15,23 +15,25 @@ export default class Row extends React.Component {
    constructor(props) {
       super(props)
 
-      if (!('index' in props)) {
-         throw TypeError('Row: Required property "index" is missing')
+      for (const requiredProp of ['index', 'callback']) {
+         if (!(requiredProp in props)) {
+            throw TypeError(`Row: Required property ${requiredProp} is missing`)
+         }
       }
    }
 
    render() {
       return (
          <tr className='Row'>
-            <Cell row={this.props.index} column={0} />
-            <Cell row={this.props.index} column={1} />
-            <Cell row={this.props.index} column={2} />
-            <Cell row={this.props.index} column={3} />
-            <Cell row={this.props.index} column={4} />
-            <Cell row={this.props.index} column={5} />
-            <Cell row={this.props.index} column={6} />
-            <Cell row={this.props.index} column={7} />
-            <Cell row={this.props.index} column={8} />
+            <Cell row={this.props.index} column={0} callback={this.props.callback} />
+            <Cell row={this.props.index} column={1} callback={this.props.callback} />
+            <Cell row={this.props.index} column={2} callback={this.props.callback} />
+            <Cell row={this.props.index} column={3} callback={this.props.callback} />
+            <Cell row={this.props.index} column={4} callback={this.props.callback} />
+            <Cell row={this.props.index} column={5} callback={this.props.callback} />
+            <Cell row={this.props.index} column={6} callback={this.props.callback} />
+            <Cell row={this.props.index} column={7} callback={this.props.callback} />
+            <Cell row={this.props.index} column={8} callback={this.props.callback} />
          </tr>
       )
    }

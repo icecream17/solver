d08314127169d5645159ae4005d473e33f154c43
steven nguyen <icecream17.github@gmail.com>
2021-05-06 11:12:16 -0500

    [skip ci] representation as array

--- src/Elems/MainElems/Sudoku.js
+++ src/Elems/MainElems/Sudoku.js
@@ -12,21 +12,70 @@ import Row from './Row';
  * <Sudoku />
  */
 export default class Sudoku extends React.Component {
+   constructor(props) {
+      super(props)
+
+      /**
+       * When the cells are proccessed they're added:
+       * ```js
+       * // In cell setup
+       * callback()
+       *
+       * // Sudoku callback
+       * this.data[cell.row][cell.column] = cell
+       * ```
+       *
+       * @name Sudoku#data
+       * @type {Cell[][]}
+       * @default {[[], [], [], [], [], [], [], [], []]}
+       */
+      this.data = []
+      for (let i = 0; i < 9; i++) {
+         this.data[i] = []
+      }
+   }
+
    render() {
       return (
-         <table className='Sudoku'>
+         <table className='Sudoku' id='Sudoku'>
             <tbody>
-               <Row index={0} />
-               <Row index={1} />
-               <Row index={2} />
-               <Row index={3} />
-               <Row index={4} />
-               <Row index={5} />
-               <Row index={6} />
-               <Row index={7} />
-               <Row index={8} />
+               <Row index={0} callback={this.updateInnerArray.bind(this)} />
+               <Row index={1} callback={this.updateInnerArray.bind(this)} />
+               <Row index={2} callback={this.updateInnerArray.bind(this)} />
+               <Row index={3} callback={this.updateInnerArray.bind(this)} />
+               <Row index={4} callback={this.updateInnerArray.bind(this)} />
+               <Row index={5} callback={this.updateInnerArray.bind(this)} />
+               <Row index={6} callback={this.updateInnerArray.bind(this)} />
+               <Row index={7} callback={this.updateInnerArray.bind(this)} />
+               <Row index={8} callback={this.updateInnerArray.bind(this)} />
             </tbody>
          </table>
       )
+   }
+
+   /** Gets the cell _element_ at the row and column. */
+   getCell(row, column) {
+      if (this.data[row][column] !== undefined) {
+         return this.data[row][column]
+      }
+      // if (this.element.current === null) {
+      //    throw new TypeError('null element??')
+      // }
+
+      return this.getRow(row).children[column]
+   }
+
+   /** Gets the row _element_ at the index provided */
+   getRow(index) {
+      if (this.element.current === null) {
+         throw new TypeError('null element??')
+      }
+
+      // this.element > tbody > row
+      return this.element.current.firstChild.children[index]
+   }
+
+   updateInnerArray(cell) {
+      this.data[cell.props.row][cell.props.column] = cell
    }
 }

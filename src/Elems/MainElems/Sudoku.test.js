cd566d8a55a69874378ddf5d0d879e28512fabb5
steven nguyen <icecream17.github@gmail.com>
2021-05-06 12:10:42 -0500

    fix reference bug

--- src/Elems/MainElems/Sudoku.test.js
+++ src/Elems/MainElems/Sudoku.test.js
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 
 import Sudoku from './Sudoku'
 
-const sudoku = <Sudoku />
-render(sudoku)
+// sudokuInstance !== sudokuElement
+const sudoku = render(<Sudoku />)
 
 test('the data callbacks were called / cells were processed', () => {
    expect(sudoku.data.flat(Infinity)).toHaveLength(64)

da5a165a00dd661ba64f20ac530ad34cbd7177aa
steven nguyen <icecream17.github@gmail.com>
2021-05-07 10:07:55 -0500

    `remove outdated test` [skip test]

--- src/todo.test.js
+++ src/todo.test.js
@@ -8,17 +8,6 @@ test('Param works', () => {
    expect(() => new Todo("something")).not.toThrow()
 })
 
-test('toString', () => {
-   if (!((new Todo()).hasOwnProperty('toString'))) {
-      expect(true).toBe(true)
-      console.warn('Remove outdated test')
-      return
-   }
-
-   expect(String(new Todo())).not.toBe('[object Object]')
-   expect(String(new Todo("something"))).not.toBe("[object Object]")
-})
-
 describe('Todo.func', () => {
    test('it exists', () => {
       expect(Todo.func).not.toBeUndefined()

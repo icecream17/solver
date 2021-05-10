c2137515bb5a1bd89a0455b6aaf7edccc5654eeb
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 11:09:50 -0500

    add todo methods

--- src/Api/solver.js
+++ src/Api/solver.js
@@ -1,4 +1,17 @@
 
 export default class Solver {
    // TODO
+   static Step () {
+      // TODO
+   }
+
+   // This is a bad name
+   // Does "Step" until it reaches the end or a strategy succeeds
+   static Run () {
+      // TODO
+   }
+   
+   static Undo () {
+      // TODO
+   }
 }

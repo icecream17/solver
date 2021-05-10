76d53503e799feb62f3c65b9134ac2dd1603c952
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 11:31:20 -0500

    add solver props [skip ci]

--- src/Elems/AsideElems/StrategyControls.js
+++ src/Elems/AsideElems/StrategyControls.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import Todo from '../../todo';
-import StrategyControl from './StrategyControl'
+import StrategyControl from './StrategyControl';
 
 /**
  * A bunch of strategy controls [TODO]
+ * @requiredProps
+ * - solver: Solver
  */
 export default class StrategyControls extends React.Component {
    render() {
       return (
          <fieldset className='StrategyControls'>
             <legend>controls (todo)</legend>
-            <StrategyControl onClick={new Todo.func("run")} name="run" />
-            <StrategyControl onClick={new Todo.func("step")} name="step" />
-            <StrategyControl onClick={new Todo.func("undo")} name="undo" />
+            <StrategyControl onClick={this.props.solver.Run} name="run" />
+            <StrategyControl onClick={this.props.solver.Step} name="step" />
+            <StrategyControl onClick={this.props.solver.Undo} name="undo" />
          </fieldset>
       )
    }

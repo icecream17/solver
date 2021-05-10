590dc594d13ff144e45d7599fe7b14639c07fb2a
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 12:50:22 -0500

    add props.solver [skip ci]

--- src/Elems/AsideElems/StrategyList.js
+++ src/Elems/AsideElems/StrategyList.js
@@ -4,29 +4,44 @@ import StrategyItem from './StrategyItem';
 
 /**
  * A list of strategies
+ *
+ * @requiredProps
+ * - solver
  */
 export default class StrategyList extends React.Component {
+   constructor(props) {
+      for (const requiredProperty of ["solver"]) {
+         if (!(requiredProperty in props)) {
+            throw TypeError(`StrategyList: Required property "${requiredProperty}" is missing`)
+         }
+      }
+
+      super(props)
+   }   
+
    render() {
       return (
          <ol className='StrategyList' id='StrategyList'>
             <StrategyItem
                name='Check for solved'
-               description={
-                  'Checks if a cell has only 1 possibility left\n' +
-                  '[todo]'
-               }
-               required='true' />
-            <StrategyItem
-               name='Example strategy'
-               description='[todo]'
+               description='Checks if a cell has only 1 possibility left\n[todo]'
+               required='true'
+               solver={this.props.solver}
             />
             <StrategyItem
-               name='Example strategy 2'
+               name='Update candidates'
                description='[todo]'
+               solver={this.props.solver}
+            />
+            <StrategyItem
+               name='Example strategy 1'
+               description='[todo]'
+               solver={this.props.solver}
             />
             <StrategyItem
                name='Another Example strategy'
                description='[todo]'
+               solver={this.props.solver}
             />
          </ol>
       )

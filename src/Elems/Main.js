1ae4cc6642e09791a7e638c70db2d609b6debc93
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 09:35:30 -0500

    pass sudoku state up [skip ci]

--- src/Elems/Main.js
+++ src/Elems/Main.js
@@ -5,14 +5,32 @@ import Sudoku from './MainElems/Sudoku'
 import Data from './MainElems/Data';
 import Coords from './MainElems/Coords';
 
-function Main() {
-   return (
-      <main className="App-main">
-         <Data />
-         <Sudoku />
-         <Coords />
-      </main>
-   );
+/**
+ * The "main" component, which includes the Data, Sudoku, and Coords components
+ *
+ * @requiredProps
+ * - whenSudokuUpdates
+ */
+class Main extends React.Component {
+   constructor(props) {
+      for (const requiredProperty of ["whenSudokuUpdates"]) {
+         if (!(requiredProperty in props)) {
+            throw TypeError(`Sudoku: Required property "${requiredProperty}" is missing`)
+         }
+      }
+
+      super(props)
+   }
+
+   render() {
+      return (
+         <main className="App-main">
+            <Data />
+            <Sudoku whenUpdate={this.props.whenSudokuUpdates} />
+            <Coords />
+         </main>
+      );
+   }
 }
 
 export default Main

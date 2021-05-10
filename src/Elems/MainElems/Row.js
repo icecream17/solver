8a54d4eb71f2df0227a82f84b6441fc647642197
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 09:55:52 -0500

    feat: renaming is complete!

--- src/Elems/MainElems/Row.js
+++ src/Elems/MainElems/Row.js
@@ -6,34 +6,38 @@ import Cell from './Cell';
 /**
  * A row in a sudoku
  *
- * Also has a required index property. {integer}
+ * @param {integer} props.index
  *
  * @example
  * <Row index={3} />
+ *
+ * @requiredProps
+ * - index
+ * - whenCellConstructs
  */
 export default class Row extends React.Component {
    constructor(props) {
-      super(props)
-
-      for (const requiredProp of ['index', 'callback']) {
+      for (const requiredProp of ['index', 'whenCellConstructs']) {
          if (!(requiredProp in props)) {
             throw TypeError(`Row: Required property ${requiredProp} is missing`)
          }
       }
+
+      super(props)
    }
 
    render() {
       return (
          <tr className='Row'>
-            <Cell row={this.props.index} column={0} callback={this.props.callback} />
-            <Cell row={this.props.index} column={1} callback={this.props.callback} />
-            <Cell row={this.props.index} column={2} callback={this.props.callback} />
-            <Cell row={this.props.index} column={3} callback={this.props.callback} />
-            <Cell row={this.props.index} column={4} callback={this.props.callback} />
-            <Cell row={this.props.index} column={5} callback={this.props.callback} />
-            <Cell row={this.props.index} column={6} callback={this.props.callback} />
-            <Cell row={this.props.index} column={7} callback={this.props.callback} />
-            <Cell row={this.props.index} column={8} callback={this.props.callback} />
+            <Cell row={this.props.index} column={0} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={1} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={2} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={3} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={4} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={5} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={6} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={7} whenConstruct={this.props.whenCellConstructs} />
+            <Cell row={this.props.index} column={8} whenConstruct={this.props.whenCellConstructs} />
          </tr>
       )
    }

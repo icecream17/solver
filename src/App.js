409e9bab90fff6d144750a48dcd56feaf8528bbe
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-10 09:40:22 -0500

    feat: lift sudoku state up

--- src/App.js
+++ src/App.js
@@ -7,17 +7,37 @@ import Version from './Elems/Version'
 import Main from './Elems/Main'
 import Aside from './Elems/Aside'
 
-function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <Title />
-            <Version />
-         </header>
-         <Main />
-         <Aside />
-      </div>
-   );
+/**
+ * The <App /> component
+ * Stores the global "state.sudoku" property
+ */
+class App extends React.Component {
+   constructor (props) {
+      super(props)
+      
+      this.state = {
+         sudoku: null
+      }
+
+      this.whenSudokuUpdates = this.whenSudokuUpdates.bind(this)
+   }
+
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <Title />
+               <Version />
+            </header>
+            <Main whenSudokuUpdates={this.whenSudokuUpdates} />
+            <Aside />
+         </div>
+      );
+   }
+
+   whenSudokuUpdates(sudoku) {
+      this.state.sudoku = sudoku.data
+   }
 }
 
 export default App

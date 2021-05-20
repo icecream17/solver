4f6ca7ba8b64685d6376bcc614a2924d5dec6ed4
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-20 09:12:03 -0500

    move twoDimensionalArray

--- src/Types.tsx
+++ src/Types.tsx
@@ -7,6 +7,8 @@ export const MAX_CELL_INDEX = 80
 export const ROW_NAMES = ["A", "B", "C", "D", "E", "F", "G", "H", "J"] as const
 export const COLUMN_NAMES = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] as const
 
+export type TwoDimensionalArray<T> = Array<Array<T>>
+
 // The following are Typescript workarounds
 
 // 1. optional whenConstruct

002de4df33e6e254c696a92138aca71e1de7ac85
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-21 19:09:32 +0000

    StrategyList > getRepeatedProps [skip ci]

--- src/Elems/AsideElems/StrategyList.tsx
+++ src/Elems/AsideElems/StrategyList.tsx
@@ -31,32 +31,35 @@ export default class StrategyList extends React.Component<StrategyListProps> {
          return index++
       }
 
+      const getRepeatedProps = () => {
+         return {
+            solver: this.props.solver,
+            index: getIndex()
+         }
+      }
+
       return (
          <ol className='StrategyList' id='StrategyList'>
             <StrategyItem
                name='Check for solved'
                description='Checks if a cell has only 1 possibility left\n[todo]'
                required='true'
-               solver={this.props.solver}
-               index={getIndex()}
+               {...getRepeatedProps()}
             />
             <StrategyItem
                name='Update candidates'
                description='[todo]'
-               solver={this.props.solver}
-               index={getIndex()}
+               {...getRepeatedProps()}
             />
             <StrategyItem
                name='Example strategy 1'
                description='[todo]'
-               solver={this.props.solver}
-               index={getIndex()}
+               {...getRepeatedProps()}
             />
             <StrategyItem
                name='Another Example strategy'
                description='[todo]'
-               solver={this.props.solver}
-               index={getIndex()}
+               {...getRepeatedProps()}
             />
          </ol>
       )

536d40e6c48a25bdfa42f673e91dbd12823e850d
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-26 10:06:29 -0500

    undo

--- src/expectProps.ts
+++ src/expectProps.ts
@@ -2,7 +2,7 @@ import React from "react"
 
 const _expect = (component: typeof React.Component, props: typeof React.Component.prototype.props) => {
    return {
-      toHaveProperty<T extends PropertyKey>(requiredProperty: T): asserts typeof props is Record<T, unknown> {
+      toHaveProperty(requiredProperty: keyof typeof props) {
          if (!(requiredProperty in props)) {
             throw TypeError(`${component.name}: Required property ${String(requiredProperty)} is missing`)
          }

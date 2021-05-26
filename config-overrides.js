086e6b09ce73607c37e6d861f30571438e3561e0
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-26 07:25:14 -0500

    debug

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 // Workaround to use logical assignment
 // until https://github.com/facebook/create-react-app/pull/10832 is merged
+// see https://github.com/facebook/create-react-app/issues/9908
 
 module.exports = {
    webpack: (config, env) => {
+      console.log(config); exit()
       const rules = config.module.rules.find(rule => !!rule.oneOf).oneOf;
       const babelLoaderRule = rules.find(rule => rule.loader.includes('babel-loader'));
 

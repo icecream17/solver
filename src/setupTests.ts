fb0d55855a405c3bc57bece55a87135e70b2f448
steven nguyen <nguyeste008@students.garlandisd.net>
2021-06-01 09:56:09 -0500

    better async stack traces

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Better async stack traces
+process.on('unhandledRejection', (reason, _promise) => {
+  throw reason
+});

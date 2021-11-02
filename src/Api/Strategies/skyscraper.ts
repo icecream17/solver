2d64dd8ca6696dc9618b0f63996eb30dd7a4bed1
icecream17 <icecream17.github@gmail.com>
2021-11-01 21:45:33 -0500

    TODO: better solution

--- src/Api/Strategies/skyscraper.ts
+++ src/Api/Strategies/skyscraper.ts
@@ -12,7 +12,7 @@ export function __incrementMapValue<T extends Map<K, number>, K>(map: T, key: K)
    }
 }
 
-export function _innerSkyscraperLogic(
+function _innerInnerSkyscraperLogic(
    candidate: SudokuDigits,
    sudoku: PureSudoku,
    sumLines: Set<CellID>,
@@ -65,6 +65,25 @@ export function _innerSkyscraperLogic(
    return null
 }
 
+function _innerSkyscraperLogic(line1: Set<CellID>, possibleLines: Set<CellID>[], candidate: SudokuDigits, sudoku: PureSudoku, isRow: boolean) {
+   // line = row/column
+   // pendLine = column/row
+   for (const line2 of possibleLines) {
+      const sumLines = new Set<CellID>()
+      line1.forEach(cell => sumLines.add(cell))
+      line2.forEach(cell => sumLines.add(cell))
+
+      if (sumLines.size < 5) {
+         const result = _innerInnerSkyscraperLogic(candidate, sudoku, sumLines, isRow, 2)
+         if (result !== null) {
+            return result
+         }
+      }
+   }
+
+   return null
+}
+
 /**
  * Disjointed x wing - see Strategies.md
  *
@@ -80,37 +99,22 @@ export default function skyscraper(sudoku: PureSudoku) {
          const row = candidateLocations[candidate].rows[index]
          const column = candidateLocations[candidate].columns[index]
 
-         const check = []
          if (row.size < 3) {
-            check.push([row, possibleRows] as const)
-            possibleRows.push(row) // Marker 1
+            const result = _innerSkyscraperLogic(row, possibleRows, candidate, sudoku, true)
+            if (result !== null) {
+               return result
+            }
+
+            possibleRows.push(row)
          }
 
          if (column.size < 3) {
-            check.push([column, possibleColumns] as const)
-            possibleColumns.push(column) // Marker 1
-         }
-
-         // line = row/column
-         // pendLine = column/row
-         for (const [line1, possibleLines] of check) {
-            for (const line2 of possibleLines) {
-               // Necessary because `Marker 1` happens before this
-               if (line1 === line2) {
-                  continue
-               }
-
-               const sumLines = new Set<CellID>()
-               line1.forEach(cell => sumLines.add(cell))
-               line2.forEach(cell => sumLines.add(cell))
-
-               if (sumLines.size < 5) {
-                  const result = _innerSkyscraperLogic(candidate, sudoku, sumLines, line1 === row, 2)
-                  if (result !== null) {
-                     return result
-                  }
-               }
+            const result = _innerSkyscraperLogic(column, possibleColumns, candidate, sudoku, true)
+            if (result !== null) {
+               return result
             }
+
+            possibleColumns.push(column)
          }
       }
    }
